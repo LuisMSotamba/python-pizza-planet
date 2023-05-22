544b673d15752c87cdadeb2339bd5acdb94ad17f
Luis Miguel Sotamba <iam.lucho.code@gmail.com>
2023-05-21 21:52:19 -0500

    test: add size test cases

--- app/test/services/test_size.py
+++ app/test/services/test_size.py
@@ -29,6 +29,9 @@ def test_get_size_by_id_service(client, create_size, size_uri):
     for param, value in current_size.items():
         pytest.assume(returned_size[param] == value)
 
+def test_get_non_existent_size_by_id_service(client, size_uri):
+    response = client.get(f'{size_uri}id/1')
+    pytest.assume(response.status.startswith('404'))
 
 def test_get_sizes_service(client, create_sizes, size_uri):
     response = client.get(size_uri)
@@ -36,3 +39,7 @@ def test_get_sizes_service(client, create_sizes, size_uri):
     returned_sizes = {size['_id']: size for size in response.json}
     for size in create_sizes:
         pytest.assume(size['_id'] in returned_sizes)
+
+def test_get_non_existent_sizes_service(client, size_uri):
+    response = client.get(size_uri)
+    pytest.assume(response.status.startswith('404'))
